766d24d560217dc0262b3393c74f213d68c2f08a
Nathan Hall <nathan.mh@gmail.com>
2016-03-21 16:28:17 -0400

    fatnaynay weather complete

--- index.ios.js
+++ index.ios.js
@@ -20,33 +20,61 @@ class firstReactiveNative extends Component {
     super(props);
     this.state = {
       zip: null,
-      forecast: {
-        main: 'Clouds',
-        description: 'Some clouds',
-        temp: 45.7
-      }
+      forecast: null
     };
   }
 
   _handleTextChange = (event) => {
     console.log(event.nativeEvent.text);
-    this.setState({zip: event.nativeEvent.text})
+    let zip = event.nativeEvent.text;
+    this.setState({zip: zip});
+    fetch('http://api.openweathermap.org/data/2.5/weather?zip=' + zip + ',us&APPID=de4f2b286e75561bf3f40b9f01ff29f2')
+      .then((response) => response.json())
+      .then((responseJSON) => {
+        console.log(responseJSON)
+        this.setState({
+          forecast: {
+            main: responseJSON.weather[0].main,
+            description: responseJSON.weather[0].description,
+            temp: responseJSON.main.temp
+          }
+        })
+      })
+      .catch((error) => {
+        console.warn(error);
+      })
   }
 
   render() {
+
+    let content = null;
+    if(this.state.forecast !== null) {
+      content = <Forecast main={this.state.forecast.main}
+                          description={this.state.forecast.description}
+                          temp={this.state.forecast.temp} />
+    }
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Your input {this.state.zip}.
-        </Text>
-        <Forecast main={this.state.forecast.main}
-                  description={this.state.forecast.description}
-                  temp={this.state.forecast.temp} />
+        <Image source={require('./img/FatNayNay.png')}
+               resizeMode='cover'
+               style={styles.backdrop}>
+          <View style={styles.overlay}>
+            <View style={styles.row}>
+              <Text style={styles.mainText}>
+                Current weather For
+              </Text>
+              <View style={styles.zipContainer}>
+                <TextInput style={[styles.zipCode, styles.mainText]}
+                           returnKeyType='go'
+                           onSubmitEditing={this._handleTextChange} />
+               </View>
+             </View>
+            {content}
 
-        <TextInput style={styles.input}
-                   returnKeyType='go'
-                   onSubmitEditing={this._handleTextChange} />
+          </View>
+        </Image>
       </View>
+
     );
   }
 }
@@ -54,14 +82,47 @@ class firstReactiveNative extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#4D4D4D',
+    paddingTop: 30
+  },
+  backdrop: {
+    flex: 1,
+    flexDirection: 'column'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+  },
+  overlay: {
+    paddingTop: 5,
+    backgroundColor: '#000',
+    opacity: 0.5,
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    alignItems: 'flex-start',
+    padding: 30
+  },
+  zipContainer: {
+    flex: 1,
+    borderBottomColor: '#DDD',
+    borderBottomWidth: 1,
+    marginLeft: 5,
+    marginTop: 3
+  },
+  zipCode: {
+    width: 50,
+    height: 16
+  },
+  mainText: {
+    flex: 1,
+    fontSize: 17,
+    color: '#fff'
   },
   input: {
     fontSize: 20,
